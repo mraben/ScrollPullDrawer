25a7e68e6c6f6a18cca158c4546283ad0cd3b732
aben <asdfghjkl@gmail.com>
2018-10-14 15:20:25 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/scrollpulldrawer/model/Address.java
@@ -0,0 +1,22 @@
+package com.example.administrator.scrollpulldrawer.model;
+
+public class Address {
+    private int imageUrl;
+    private String desContent;
+
+    public int getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(int imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getDesContent() {
+        return desContent;
+    }
+
+    public void setDesContent(String desContent) {
+        this.desContent = desContent;
+    }
+}
